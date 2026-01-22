afe041fd0ae8066c4e24ebd7eac24d90303908c6
Aike Fernandez Roza <aroza@izertis.com>
2026-01-22 20:33:55 +0100

    Player Jump and Run

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,20 +14,44 @@ public class PlayerController : MonoBehaviour
     
     Vector2 rawMovement = Vector2.zero;
     
+    private Vector3 velocity;
+    private bool isGrounded;
+    
     [Header("Movement")]
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float runMultiplier = 5f;
     [SerializeField] private float lookSpeed = 1f;
+    
+    [Header("Jump")]
+    [SerializeField] private float jumpHeight = 2f;
     
     [Header("Camera")]
     [SerializeField] private Camera playerCamera;
+    
+    private CharacterController characterController;
+    private Rigidbody playerRigidbody;
 
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+    
     private void OnEnable()
     {
         move.action.Enable();
+        jump.action.Enable();
+        run.action.Enable();
 
         move.action.started += OnMove;
         move.action.performed += OnMove;
         move.action.canceled += OnMove;
+
+        jump.action.started += OnJump;
+        
+        run.action.started += OnRun;
+        run.action.performed += OnRun;
+        run.action.canceled += OnRun;
     }
 
     // Events
@@ -36,12 +60,45 @@ public class PlayerController : MonoBehaviour
         rawMovement = ctx.ReadValue<Vector2>();
     }
 
+    private bool mustJump = false;
+    private void OnJump(InputAction.CallbackContext ctx)
+    {
+        mustJump = ctx.ReadValueAsButton();
+        // Debug.Log($"mustJump: {mustJump}");
+    }
+
+    private bool mustRun= false;
+    private void OnRun(InputAction.CallbackContext ctx)
+    {
+        mustRun = ctx.ReadValueAsButton();
+    }
+    
     // Update
     private void Update()
     {
-        float moveSpeed = speed * Time.deltaTime;
-        Vector3 moveToApply = new Vector3(rawMovement.x * moveSpeed, 0, rawMovement.y * moveSpeed);
-        transform.Translate(moveToApply);
+        isGrounded = characterController.isGrounded;
+        
+        float realSpeed = speed;
+
+        if (mustRun)
+        {
+            realSpeed = speed * runMultiplier;
+        }
+        
+        Vector3 moveToApply = new Vector3(rawMovement.x, 0 , rawMovement.y);
+        characterController.Move(moveToApply * realSpeed * Time.deltaTime);
+        
+        // Jump
+        if (mustJump && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(2f * Mathf.Abs(-Physics.gravity.y) * jumpHeight);
+            mustJump = false;
+        }
+        
+        // Aplicar gravedad
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+
     }
     
     private void OnDisable()
@@ -49,5 +106,15 @@ public class PlayerController : MonoBehaviour
         move.action.started -= OnMove;
         move.action.performed -= OnMove;
         move.action.canceled -= OnMove;
+
+        jump.action.started -= OnJump;
+        
+        run.action.started -= OnRun;
+        run.action.performed -= OnRun;
+        run.action.canceled -= OnRun;
+        
+        move.action.Disable();
+        jump.action.Disable();
+        run.action.Disable();
     }
 }
