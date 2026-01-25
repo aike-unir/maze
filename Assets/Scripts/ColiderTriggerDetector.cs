f6aea228183b832d35c4e86712db93169e155079
Aike Fernandez Roza <aroza@izertis.com>
2026-01-25 18:35:11 +0100

    Damage, life and gameover restart

--- Assets/Scripts/ColiderTriggerDetector.cs
+++ Assets/Scripts/ColiderTriggerDetector.cs
@@ -1,29 +1,72 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ColiderTriggerDetector : MonoBehaviour
 {
+    [SerializeField] private int initialLife = 3;
+    
     [SerializeField] private float radius = 1.1f;
     [SerializeField] private Canvas canvasTrapDetected;
     [SerializeField] private LayerMask layerAllowed;
+    
+    [SerializeField] public Image[] heartsImages;
     // estas layer mask es para comprobar solo una layer, dicha layer la podemos poner en
     // ProjectSettings / physics / Layer Collision Matrix / (desactivar la layer para no tener que calcularla constantemente)
+
+    private int life;
+
+    private void Awake()
+    {
+        life = initialLife;
+    }
     
+    float timer = 0f;
     private void Update()
     {
+        timer += Time.deltaTime;
+        
         Collider[] colliders = Physics.OverlapSphere(transform.position, radius, layerAllowed);
         bool anyTrapDetected = false;
         foreach (Collider col in colliders)
         {
-            
-            if (col.tag == "Trap")
+            if (timer >= 5f)
             {
-                anyTrapDetected = true;
-                Debug.Log("Hay un trigger trampa cerca");
+                if (col.tag == "Trap")
+                {
+                    anyTrapDetected = true;
+                    Debug.Log("Hay un trigger trampa cerca");
+
+                    life--;
+                    timer = 0f;
+                    Debug.Log($"Vida disminuye a {life}");
+                    AdjustLife(life);
+                }
             }
-            
         }
 
         canvasTrapDetected.gameObject.SetActive(anyTrapDetected);
 
+    }
+
+    private void AdjustLife(int life)
+    {
+        if (life <= 0)
+        {
+            RestartGame();
+        }
+        else
+        {
+            for (int i = 0; i < heartsImages.Length; i++)
+            {
+                heartsImages[i].enabled = life > i;
+            }
+        }
+
+    }
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
     }
 }
